98c7922193fc62f30ad2df8005a7c4504f098db1
Kirilovich Arseniy <94453515+SenchaBrest@users.noreply.github.com>
2025-08-25 18:18:56 +0300

    Update data_container.py

--- utils/data_container.py
+++ utils/data_container.py
@@ -127,6 +127,7 @@ class AccidentDataset(Dataset):
         g = dgl.DGLGraph(sub_graph)
 
         # get temporal_features (speed)
+        accident_time = pd.to_datetime(accident_time)
         date_range = pd.date_range(end=accident_time.strftime("%Y%m%d %H"), freq="1H", periods=24)
         selected_time = self.speed.loc[date_range]
 
